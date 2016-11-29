d6266da84722c0eb01a8b31c54f110d3cf832332
NamrathaKanthi <namratha.k7@gmail.com>
2016-11-29 11:47:38 +0530

    static page

---
+++ static page/Angular/JS/backend.js
@@ -0,0 +1,5 @@
+var app = angular.module('myApp',[]);
+app.controller('myCntrl',function($scope){
+//console.log($scope.name);
+$scope.names = ["namratha","latha","varudhini","vamsi","raghavendra","praneeth","deepu","vaishu"];
+});
