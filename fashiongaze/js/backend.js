126b19413b9c21388bcbb17d60a890c0ee0ddfca
NamrathaKanthi <namratha.k7@gmail.com>
2016-12-06 16:09:26 +0530

    new fashiongaze

--- fashiongaze/js/backend.js
+++ fashiongaze/js/backend.js
@@ -14,3 +14,39 @@ app.controller('myCntrl',function($scope){
 	}
 
 });
+app.directive('firstdiv',function(){
+	return{
+		restrict:"E",
+		templateUrl:"html/firstdiv.html"
+	}
+});
+app.directive('secondiv',function(){
+	return{
+		restrict:"E",
+		templateUrl:"html/secondiv.html"
+	}
+});
+app.directive('thirdiv',function(){
+	return{
+		restrict:"E",
+		templateUrl:"html/thirdiv.html"
+	}
+});
+app.directive('fourthdiv',function(){
+	return{
+		restrict:"E",
+		templateUrl:"html/fourthdiv.html"
+	}
+});
+app.directive('fifthdiv',function(){
+	return{
+		restrict:"E",
+		templateUrl:"html/fifthdiv.html"
+	}
+});
+app.directive('carsouel',function(){
+	return{
+		restrict:"E",
+		templateUrl:"html/carsouel.html"
+	}
+});
