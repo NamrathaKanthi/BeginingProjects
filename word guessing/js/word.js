67288cea9378b33a75d8f418b98d6efddf77e9f9
NamrathaKanthi <namratha.k7@gmail.com>
2016-12-23 17:57:40 +0530

    word puzzle

--- word guessing/js/word.js
+++ word guessing/js/word.js
@@ -3,6 +3,24 @@ app.controller('WordGuessCtrl',function($scope){
 	var wordArray=['bootstrap','phython','angular','sass','opera','css'];
 	$scope.enterletter = "";
 	$scope.wrongword=[];
+    
+    $scope.inputvalue= function($event){
+        var currentevent= $event.currentTarget;
+        console.log(currentevent);
+        var currenteventparent= angular.element(currentevent).parent().children();
+        console.log(currenteventparent);
+        var currenteventbutton= currenteventparent[1];
+        console.log(currenteventbutton);
+        $scope.active=angular.element(document.getElementsByClassName('guessButton')[0]).addClass('active');
+        //console.log(x);
+    }
+    $scope.EnterKey = function($event){
+    $scope.active = $event.which || $event.keyCode;
+    if ($scope.active === 13) {
+        //alert("hello");
+    }
+  };
+
 	$scope.guess=function(enterletter){
 		var indicesArray = [];
 		for(var i=0;i<$scope.randomWord.length;i++){
@@ -36,13 +54,18 @@ app.controller('WordGuessCtrl',function($scope){
             $scope.enterletter = "";
         }
     }
+
+    
+
 	$scope.getRandomWord=function(){
 			$scope.randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
 			console.log($scope.randomWord);
         	$scope.z = [];
         	for (var j = 0; j < $scope.randomWord.length; j++) {
             $scope.z.push({ 'name':$scope.randomWord[j], 'value': '' });
+            console.log($scope.z);
         	}
+
             $scope.wrongword=[];
 		 console.log($scope.z[0].value);
 	};
